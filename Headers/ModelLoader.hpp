1465a09d0783222e2f6b9c391d778a0f3bbdea54
DiligentNetrunner <JadTala1621@gmail.com>
2021-01-31 19:53:05 +0100

    Initial commit

--- Headers/ModelLoader.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "VertexBuffers.hpp"
-
-namespace ddls
-{
-void loadObjModel(std::vector<Vertex>& vertices, std::vector<uint32_t>& indices);
-}
--- Headers/RenderPass.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <vulkan/vulkan.hpp>
-
-namespace ddls
-{
-void createRenderPass(VkDevice device, VkPhysicalDevice physicalDevice, VkRenderPass& renderPass, VkFormat swapChainImageFormat);
-}
