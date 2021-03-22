c5f9926f2f2c6658c568855cf13f13ee74410f74
Jad Tala <JadTala1621@gmail.com>
2021-03-23 00:01:03 +0100

    Vertex input layout

--- Daedalus/Mesh.cpp
+++ Daedalus/Mesh.cpp
@@ -1,1 +1,40 @@
 #include "Mesh.h"
+
+VertexInputDescription Vertex::get_vertex_description() {
+    VertexInputDescription description;
+
+    //we will have just 1 vertex buffer binding, with a per-vertex rate
+    VkVertexInputBindingDescription mainBinding = {};
+    mainBinding.binding = 0;
+    mainBinding.stride = sizeof(Vertex);
+    mainBinding.inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
+
+    description.bindings.push_back(mainBinding);
+
+    //Position will be stored at Location 0
+    VkVertexInputAttributeDescription positionAttribute = {};
+    positionAttribute.binding = 0;
+    positionAttribute.location = 0;
+    positionAttribute.format = VK_FORMAT_R32G32B32_SFLOAT;
+    positionAttribute.offset = offsetof(Vertex, position);
+
+     //Normal will be stored at Location 1
+    VkVertexInputAttributeDescription normalAttribute = {};
+    normalAttribute.binding = 0;
+    normalAttribute.location = 1;
+    normalAttribute.format = VK_FORMAT_R32G32B32_SFLOAT;
+    normalAttribute.offset = offsetof(Vertex, normal);
+
+    //Color will be stored at Location 2
+    VkVertexInputAttributeDescription colorAttribute = {};
+    colorAttribute.binding = 0;
+    colorAttribute.location = 2;
+    colorAttribute.format = VK_FORMAT_R32G32B32_SFLOAT;
+    colorAttribute.offset = offsetof(Vertex, color);
+
+    description.attributes.push_back(positionAttribute);
+    description.attributes.push_back(normalAttribute);
+    description.attributes.push_back(colorAttribute);
+    
+    return description;
+}
--- Daedalus/Mesh.h
+++ Daedalus/Mesh.h
@@ -6,10 +6,19 @@
 
 #include <vector>
 
+struct VertexInputDescription {
+    std::vector<VkVertexInputBindingDescription> bindings;
+    std::vector<VkVertexInputAttributeDescription> attributes;
+
+    VkPipelineVertexInputStateCreateFlags flags = 0;
+};
+
 struct Vertex {
     glm::vec3 position;
     glm::vec3 normal;
     glm::vec3 color;
+    
+    static VertexInputDescription get_vertex_description();
 };
 
 struct Mesh {
