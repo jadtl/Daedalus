04866abec81ca58958cc30fed0204e4d44086b84
Jad Tala <JadTala1621@gmail.com>
2021-03-23 21:31:35 +0100

    Image view create info

--- Daedalus/Initializers.cpp
+++ Daedalus/Initializers.cpp
@@ -135,3 +135,21 @@ VkImageCreateInfo init::imageCreateInfo(VkFormat format, VkImageUsageFlags usage
 
     return info;
 }
+
+VkImageViewCreateInfo init::imageViewCreateInfo(VkFormat format, VkImage image, VkImageAspectFlags aspectFlags) {
+    //build a image-view for the depth image to use for rendering
+    VkImageViewCreateInfo info{};
+    info.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+    info.pNext = nullptr;
+
+    info.viewType = VK_IMAGE_VIEW_TYPE_2D;
+    info.image = image;
+    info.format = format;
+    info.subresourceRange.baseMipLevel = 0;
+    info.subresourceRange.levelCount = 1;
+    info.subresourceRange.baseArrayLayer = 0;
+    info.subresourceRange.layerCount = 1;
+    info.subresourceRange.aspectMask = aspectFlags;
+
+    return info;
+}
