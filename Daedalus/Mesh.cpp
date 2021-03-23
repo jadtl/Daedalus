b021f6f1936a4b2cf038f2c6c386ac03d8869051
Jad Tala <JadTala1621@gmail.com>
2021-03-23 13:29:57 +0100

    Assets and shaders script update

--- Daedalus/Mesh.cpp
+++ Daedalus/Mesh.cpp
@@ -1,5 +1,6 @@
 #include "Mesh.h"
 
+#define TINYOBJLOADER_IMPLEMENTATION
 #include "OBJLoader.h"
 
 #include <iostream>
@@ -68,5 +69,48 @@ bool Mesh::loadFromObj(const char *fileName) {
         return false;
     }
     
+    // Loop over shapes
+    for (size_t s = 0; s < shapes.size(); s++) {
+        // Loop over faces(polygon)
+        size_t index_offset = 0;
+        for (size_t f = 0; f < shapes[s].mesh.num_face_vertices.size(); f++) {
+
+            //hardcode loading to triangles
+            int fv = 3;
+
+            // Loop over vertices in the face.
+            for (size_t v = 0; v < fv; v++) {
+                // access to vertex
+                tinyobj::index_t idx = shapes[s].mesh.indices[index_offset + v];
+
+                //vertex position
+                tinyobj::real_t vx = attrib.vertices[3 * idx.vertex_index + 0];
+                tinyobj::real_t vy = attrib.vertices[3 * idx.vertex_index + 1];
+                tinyobj::real_t vz = attrib.vertices[3 * idx.vertex_index + 2];
+                //vertex normal
+                tinyobj::real_t nx = attrib.normals[3 * idx.normal_index + 0];
+                tinyobj::real_t ny = attrib.normals[3 * idx.normal_index + 1];
+                tinyobj::real_t nz = attrib.normals[3 * idx.normal_index + 2];
+                
+                //copy it into our vertex
+                Vertex new_vert;
+                new_vert.position.x = vx;
+                new_vert.position.y = vy;
+                new_vert.position.z = vz;
+
+                new_vert.normal.x = nx;
+                new_vert.normal.y = ny;
+                new_vert.normal.z = nz;
+
+                //we are setting the vertex color as the vertex normal. This is just for display purposes
+                new_vert.color = new_vert.normal;
+
+                
+                vertices.push_back(new_vert);
+            }
+            index_offset += fv;
+        }
+    }
+
     return true;
 }
