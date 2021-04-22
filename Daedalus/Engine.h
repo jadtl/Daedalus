af602ae5e86e427863698e369de8b9793a897a5e
Jad Tala <JadTala1621@gmail.com>
2021-04-22 22:19:26 +0000

    Implemented base engine class

--- Daedalus/Engine.h
+++ Daedalus/Engine.h
@@ -1,107 +1,24 @@
-#pragma once
-
-#include <glm/glm.hpp>
-
-#if defined(_WIN32)
-    #include <Windows.h>
-#elif defined(__APPLE__)
-    #include <MoltenVK/mvk_vulkan.h>
-#endif
-
-#include "Platform.h"
-#include "Types.h"
-#include "Mesh.h"
-
-#include "Explorer.h"
-#include "Console.h"
-
-#include <vector>
 #include <string>
-#include <deque>
+#include <vector>
 
-struct MeshPushConstants {
-    glm::vec4 data;
-    glm::mat4 renderMatrix;
-};
-
-struct DeletionQueue {
-    std::deque<std::function<void()>> deletors;
-    
-    void push_function(std::function<void()>&& function) { deletors.push_back(function); }
-
-    void flush() {
-        // Reverse iterate the deletion queue to execute all the functions
-        for (auto it = deletors.rbegin(); it != deletors.rend(); it++) {
-            (*it)(); // Call functions
-        }
-        deletors.clear();
-    }
-};
+class Platform;
 
 class Engine {
 public:
-    Engine(const Engine &engine) = delete;
-    Engine &operator = (const Engine &engine) = delete;
-    
-    Engine(const std::vector<std::string> &args, void *windowHandle);
-    ~Engine();
-    
-    Explorer *explorer;
-    Console console;
-    void check(std::string section, std::string action, bool result, bool silentSuccess = false);
-    void check(std::string section, std::string action, VkResult result, bool silentSuccess = false);
-    
-    VkInstance instance;
-    VkDebugUtilsMessengerEXT debugMessenger;
-    VkPhysicalDevice physicalDevice;
-    VkDevice device;
-    VkSurfaceKHR surface;
-    
-    VkSwapchainKHR swapchain;
-    VkFormat swapchainImageFormat;
-    std::vector<VkImage> swapchainImages;
-    std::vector<VkImageView> swapchainImageViews;
-    
-    VkQueue graphicsQueue;
-    uint32_t graphicsQueueFamily;
-    
-    VkCommandPool commandPool;
-    VkCommandBuffer mainCommandBuffer;
-    VkRenderPass renderPass;
-    std::vector<VkFramebuffer> framebuffers;
-    
-    VkSemaphore presentSemaphore, renderSemaphore;
-    VkFence renderFence;
-    
-    VkShaderModule vertexShader;
-    VkShaderModule fragmentShader;
-    
-    VkPipeline pipeline;
-    VkPipelineLayout pipelineLayout;
-    Mesh triangleMesh;
-    Mesh monkeyMesh;
-    
-    VkImageView depthImageView;
-    AllocatedImage depthImage;
-
-    VkFormat depthFormat;
-    
-    VmaAllocator allocator;
-    DeletionQueue mainDeletionQueue;
-    
-    bool isInitialized{false};
-    int frameNumber{0};
+    Engine(const Engine& engine) = delete;
+    Engine &operator=(const Engine& engine) = delete;
+    virtual ~Engine() {}
     
     struct Settings {
-        const char* engineName;
         const char* applicationName;
+        const char* engineName;
         
-        VkExtent2D windowExtent;
+        std::vector<int> windowExtent;
         
-        bool validate{false};
-        bool verbose{false};
+        bool validate;
+        bool verbose;
     };
-    Settings settings;
+    const Settings &settings() const { return settings_; }
     
     enum Key {
         KEY_A,
@@ -114,29 +31,36 @@ public:
     };
     void onKey(Key key);
     
-    void initialize();
+    virtual void onTick() {}
+    virtual void onFrame() {}
     
-    void update();
-    void render();
+    virtual void update() {}
+    
+protected:
+    Engine(const char* applicationName, const std::vector<std::string>& args) : settings_(), platform_(nullptr) {
+        settings_.applicationName = applicationName;
+        settings_.engineName = "Engine";
+        
+        settings_.windowExtent = { 1280, 1024 };
+        
+        settings_.validate = false;
+        settings_.verbose = false;
+        
+        parseArgs(args);
+    }
+    
+    Settings settings_;
+    Platform *platform_;
     
 private:
-    void* windowHandle;
-    
-    void initializeVulkan();
-    void initializeSwapchain();
-    void initializeCommands();
-    void initializeDefaultRenderPass();
-    void initializeFramebuffers();
-    void initializeSyncStructures();
-    void initializePipelines();
-    
-    void terminate();
-    void terminateSwapchain();
-    
-    void updateSwapchain();
-    
-    bool loadShaderModule(const char* filePath, VkShaderModule* shaderModule);
-    void loadMeshes();
-    
-    void uploadMesh(Mesh &mesh);
+    void parseArgs(const std::vector<std::string>& args) {
+        for (auto iterator = args.begin(); iterator != args.end(); ++iterator) {
+            if (*iterator == "-validate" || *iterator == "-v")
+                settings_.validate = true;
+            if (*iterator == "-verbose" || *iterator == "-vv") {
+                settings_.validate = true;
+                settings_.verbose = true;
+            }
+        }
+    }
 };
