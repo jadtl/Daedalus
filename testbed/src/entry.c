153808b6a5fd9321d7e913e243d30a0dc1ccafb5
Jad Tala <JadTala1621@gmail.com>
2021-05-23 16:09:24 +0200

    Entry point update

--- testbed/src/entry.c
+++ testbed/src/entry.c
@@ -9,7 +9,7 @@ b8 create_game(game* out_game) {
   out_game->app_config.start_pos_y = 100;
   out_game->app_config.start_width = 1200;
   out_game->app_config.start_height = 720;
-  out_game->app_config.name = "Daedalus Engine";
+  out_game->app_config.name = "Daedalus (Testbed)";
   out_game->update = game_update;
   out_game->render = game_render;
   out_game->initialize = game_initialize;
