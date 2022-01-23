c8bbc10a9dffccf7f2fc7a6c3620c8128882cbc3
Jad Tala <JadTala1621@gmail.com>
2022-01-23 22:28:02 +0100

    (API) Updated project structure

--- Engine/src/daedalus/memory.cpp
+++
@@ -1,9 +0,0 @@
-#include <daedalus/memory.h>
-
-#include <memory>
-
-namespace ddls {
-    void* zero_memory(void* block, u64 size) {
-        return memset(block, 0, size);
-    }
-}
--- Engine/src/daedalus/memory.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <defines.h>
-
-namespace ddls {
-    DDLS_API void* zero_memory(void* block, u64 size);
-}
