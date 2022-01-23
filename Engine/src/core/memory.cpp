5396871063e8028a21def5e6bbf7089c9980bb2f
Jad Tala <JadTala1621@gmail.com>
2022-01-23 22:26:35 +0100

    (Core) Memory helper functions

---
+++ Engine/src/core/memory.cpp
@@ -0,0 +1,9 @@
+#include <core/memory.h>
+
+#include <memory>
+
+namespace ddls {
+void* zero_memory(void* block, u64 size) {
+    return memset(block, 0, size);
+}
+}
---
+++ Engine/src/core/memory.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <defines.h>
+
+namespace ddls {
+DDLS_API void* zero_memory(void* block, u64 size);
+}
