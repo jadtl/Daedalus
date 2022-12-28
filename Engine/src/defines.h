91ef156453d6c7b3e95c855c516889371d4daa27
jadtl <03-grapple-very@icloud.com>
2022-12-28 01:47:59 -0800

    Add function visibility

--- Engine/src/defines.h
+++ Engine/src/defines.h
@@ -5,4 +5,20 @@ namespace ddls {
 /** @brief Aknowledges that the given values are unused inside a function */
 template<class T> void ignore(const T&...) {}
 
+#ifdef DDLS_EXPORT
+// Exports
+#ifdef _MSC_VER
+#define DDLS_API __declspec(dllexport)
+#else
+#define DDLS_API __attribute__((visibility("default")))
+#endif
+#else
+// Imports
+#ifdef _MSC_VER
+#define DDLS_API __declspec(dllimport)
+#else
+#define DDLS_API
+#endif
+#endif
+
 } // namespace ddls
