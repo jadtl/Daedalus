9c9cc34aec6c6d1f134bfc8408848630429d99a2
jadtl <03-grapple-very@icloud.com>
2023-01-17 23:00:43 +0100

    Revert "Update main header"
    This reverts commit 0f192293b28f48fc1f964b6bb31706535570f64c.

--- Engine/src/daedalus.h
+++ Engine/src/daedalus.h
@@ -3,14 +3,11 @@
 #include "core/assert.h"
 #include "core/debug.h"
 #include "core/defines.h"
+#include "core/types.h"
+#include "core/memory.h"
 #include "core/error.h"
 #include "core/log.h"
-#include "core/memory.h"
-#include "core/types.h"
 
-#include "graphics/gui.h"
-#include "graphics/pipeline.h"
 #include "graphics/renderer.h"
-#include "graphics/swapchain.h"
 
 #include "utils/endian.h"
